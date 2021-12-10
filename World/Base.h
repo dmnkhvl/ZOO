66bd74f234455aebf57711034917e5230b6f07bd
dmnkhvl <dmnkhvl@gmail.com>
2021-12-10 14:38:56 +0100

    Added Base

---
+++ World/Base.h
@@ -0,0 +1,14 @@
+//
+// Created by Dominik Havel on 10/12/2021.
+//
+
+#ifndef BUDOVATELSKA_HRA_ZOOPROJEKT_BASE_H
+#define BUDOVATELSKA_HRA_ZOOPROJEKT_BASE_H
+
+
+class Base {
+
+};
+
+
+#endif //BUDOVATELSKA_HRA_ZOOPROJEKT_BASE_H
